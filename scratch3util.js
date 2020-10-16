225bdcbce2e1cd8eac05b0f7d2cc9c49fc64c4ac
powerbox1000 <72870790+powerbox1000@users.noreply.github.com>
2020-10-16 09:35:42 -0400

    Add files via upload

---
+++ scratch3util.js
@@ -0,0 +1,21 @@
+class scratchutil {
+    getInfo() {
+        return {
+            "id": "scratchutil",
+            "name": "Scratch Utilities",
+            "blocks": [{
+                    "opcode": "openURL",
+                    "blockType": "reporter",
+                    "text": "open secure URL [address] in new window",
+                    "arguments": {
+                        "address": {
+                            "type": "string",
+                            "defaultValue": "scratch.mit.edu"
+                        }
+                    }
+                },
+            }],
+        "menus": {}
+    };
+}
+Scratch.extensions.register(new scratchutil());
